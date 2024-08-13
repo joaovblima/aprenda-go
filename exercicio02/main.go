42972da94883178afc5998118a72948378a63191
João Lima <joaovictorbernardinodelima@gmail.com>
2024-08-13 20:26:41 -0300

    adicioa exercicio

---
+++ exercicio02/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x int 
+var y string 
+var z bool
+
+func main() {
+	fmt.Printf("%T, %v\n", x, x)
+	fmt.Printf("%T, %v\n", y, y)
+	fmt.Printf("%T, %v\n", z, z)
+}
